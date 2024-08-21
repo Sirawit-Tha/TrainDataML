b25bd8500c222c339110afdcbc3c15b99951f506
Sirawit-Tha <157862772+Sirawit-Tha@users.noreply.github.com>
2024-08-21 14:52:17 +0700

    Update main.py

--- main.py
+++ main.py
@@ -22,18 +22,18 @@ if classifier == 'KNN' :
   knn = KNeighborsClassifier(n_neighbors=10)
   knn.fit(x_train, y_train)
   y_pred = knn.predict(x_test)
-  accuracy_score(y_test, y_pred)
+  acc = accuracy_score(y_test, y_pred)
   st.write(acc)
 if classifier == 'SVM' :
   svm = SVC()
   svm.fit(x_train, y_train)
   y_pred = svm.predict(x_test)
-  accuracy_score(y_test, y_pred)
+  acc = accuracy_score(y_test, y_pred)
   st.write(acc)
 if classifier == 'Decision Tree' :
   dt = DecisionTreeClassifier()
   dt.fit(x_train, y_train)
   y_pred = dt.predict(x_test)
-  accuracy_score(y_test, y_pred)
+  acc = accuracy_score(y_test, y_pred)
   st.write(acc)
 
