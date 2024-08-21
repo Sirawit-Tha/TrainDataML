c6d05a6acca0394e425e4fc24e96beb002bbd453
Sirawit-Tha <157862772+Sirawit-Tha@users.noreply.github.com>
2024-08-21 16:09:39 +0700

    Update main2.py

--- main2.py
+++ main2.py
@@ -1,3 +1,7 @@
+import numpy as np
+import streamlit as st
+import seaborn as sns
+import matplotlib.pyplot as plt
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.svm import SVR
 from sklearn.neural_network import MLPRegressor
